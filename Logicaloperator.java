d27ddfe69415926649aaa4619452f067a0d4935b
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-06-24 12:16:50 +0530

    operator file added

---
+++ Logicaloperator.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class logicaloperator {
+    public static void operators(boolean a, boolean b) {
+        // Do a AND b below
+        boolean p = (a && b);
+            // Do a OR b below
+            boolean q =(a || b);
+                // Do NOT a below
+            boolean r = (!a);
+            System.out.println(p + " " + q + " " + r);
+    }
+}
