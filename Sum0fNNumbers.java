fee64196737aef505ac293486a2ad186193a582b
Subhrasis-168 <subhrasisbiswal753@gmail.com>
2025-06-26 12:16:34 +0530

    Update Sum0fNNumbers.java

--- Sum0fNNumbers.java
+++ Sum0fNNumbers.java
@@ -2,7 +2,7 @@ class SumOfNNumbers {
     public static int nSum(int n) {
         // code here
         
-        int ans = n * (n+1)/1;
+        int ans = n * (n+1)/2;
         return ans;
     }
 }
