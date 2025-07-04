82fa4b5371e11a41243a319fda26144ed2d9d8c8
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-07-04 11:34:35 +0530

    todays practice problem

---
+++ TheElseStatement.java
@@ -0,0 +1,10 @@
+
+class TheElseStatement{
+    public static void utility(int number) {
+        if(number > 100){
+            System.out.println("Big");
+        }else if(number <= 100){
+            System.out.println("Number");
+        }
+    }
+}
