8f119189612d4d4bbe7d1e78052ce8c2c92f494e
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-07-09 11:48:51 +0530

    loop problem

---
+++ ForLoop2.java
@@ -0,0 +1,8 @@
+class ForLoop1 {
+    public static void utility(String s) {
+        // code here
+        for(int i = 0; i < s.length(); i +=2){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
