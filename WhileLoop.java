93a728ddd15d736497ee5531376ce49a12fc1ba4
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-07-09 11:49:32 +0530

    loop problem

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+class WhileLoop {
+    public static void utility(int x) {
+        // code here
+        int i = 0;
+        while(x >= i){
+            System.out.print(x + " ");
+            x--;
+        }
+    }
+}
