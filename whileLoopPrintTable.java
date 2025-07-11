9e7837248cf5eb0fd7b8c12a6fc01e0ff64bbd56
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-07-11 20:43:20 +0530

    Print Table

---
+++ whileLoopPrintTable.java
@@ -0,0 +1,8 @@
+class whileLoopPrintTable  {
+    public void calculateMultiples(int n) {
+        // code here
+        for(int i = 10; i >= 1;i--){
+            System.out.print(n * i + " ");
+        }
+    }
+}
