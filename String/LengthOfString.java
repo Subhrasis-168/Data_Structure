3098afa8bb45dfdc7b69631215db34b2147169f7
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-03 19:45:39 +0530

    String

---
+++ String/LengthOfString.java
@@ -0,0 +1,12 @@
+package String;
+
+// User function Template for Java
+
+class LengthOfString {
+    // Complete the function
+    // str: input string
+    public static int lengthString(String str) {
+        // find the length of string
+        return str.length();
+    }
+}
