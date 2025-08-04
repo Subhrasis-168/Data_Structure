00fdceb531bb33cdcc48a697d5129ae179eba891
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-04 23:48:22 +0530

    String

---
+++ String/CheckPalindrome.java
@@ -0,0 +1,20 @@
+package String;
+
+class CheckPalindrome {
+    public static boolean isPalindrome(String s) {
+        // code here
+        s = s.toLowerCase();
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
