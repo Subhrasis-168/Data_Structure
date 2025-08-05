063ef1154bee474bf01ddc49f0ef9ef7400b80a2
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-05 20:52:31 +0530

    Class and Objects

---
+++ ClassAndObject/DesignAClass.java
@@ -0,0 +1,10 @@
+package ClassAndObject;
+// User function Template for Java
+
+// Design a class as described in the question
+// Code here
+class MyClass{
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
