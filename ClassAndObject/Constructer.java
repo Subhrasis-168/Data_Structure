c7315101fb1b4aa3588d15a834649d0707c99481
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-07 20:08:45 +0530

    Constructer

---
+++ ClassAndObject/Constructer.java
@@ -0,0 +1,16 @@
+package ClassAndObject;
+
+// User function Template for Java
+
+// design a class User
+// instance variable: name(String)
+// Constructor: no parameter, initialize name to "Default"
+
+// code here
+class Constructer{
+
+        String name;
+        Constructer(){
+            name = "Default";
+        }
+    }
