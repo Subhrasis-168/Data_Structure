42c1eeb1af14b6751d3393b513ffa17b2a821c26
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-07 20:10:51 +0530

    Constructer

---
+++ ClassAndObject/ConstructerOverloading.java
@@ -0,0 +1,17 @@
+package ClassAndObject;
+
+// User function Template for Java
+
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+class ConstructerOverloading{
+    String name;
+    ConstructerOverloading(){
+        name = "Default";
+    }
+    ConstructerOverloading(String name){
+        this.name = name;
+    }
+}
