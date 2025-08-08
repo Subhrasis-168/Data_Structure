2415931e5fd46cddabcfb2e08175ae002b5689ed
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-08 21:25:38 +0530

    ArrayList

---
+++ ArrayListProgram/ArrayListTraversal.java
@@ -0,0 +1,10 @@
+package ArrayListProgram;
+
+class ArrayListTraversal {
+    public static void listTraversal(ArrayList<Integer> list) {
+        // code here
+        for(int i = 0; i<list.size();i++){
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
