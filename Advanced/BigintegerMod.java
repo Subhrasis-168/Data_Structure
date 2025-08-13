416a5606ac217a35cb9413116bcc4caa1d09611e
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-13 19:47:42 +0530

    Advanced Java

---
+++ Advanced/BigintegerMod.java
@@ -0,0 +1,16 @@
+package Advanced;
+
+/*
+ * Class MathematicalOperation with mod function
+ * a, b : Two parameters of BigIntegers to find mod
+ */
+import java.math.BigInteger;
+
+class BigintegerMod {
+
+    // mod function to find the modulus of b by a
+    static BigInteger mod(BigInteger a, BigInteger b) {
+        // Your Code here
+        return b.mod(a);
+    }
+}
