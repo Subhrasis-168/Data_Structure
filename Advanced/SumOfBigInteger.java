e95685b90d7a594597fd05145e7973a9a52a390e
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-13 19:45:06 +0530

    Advanced Java

---
+++ Advanced/SumOfBigInteger.java
@@ -0,0 +1,15 @@
+package Advanced;
+
+/* Class Solution with add fucntion
+ * a, b : Two parameters of BigIntegers to add
+ */
+import java.math.BigInteger;
+
+class SumOfBigInteger {
+
+    static BigInteger add(BigInteger a, BigInteger b) {
+
+        // Your code here
+        return a.add(b);
+    }
+}
