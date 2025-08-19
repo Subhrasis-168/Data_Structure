faa58439a124d4183cefc04a66e976085714aeb5
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-19 20:30:17 +0530

    Algorithm Basics

---
+++ ARRAY/ArrayInsertAtIndex.java
@@ -0,0 +1,11 @@
+package ARRAY;
+
+import java.util.*;
+
+class ArrayInsertAtIndex {
+    public void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        // code here
+        arr.add(index, val);
+    }
+}
+
