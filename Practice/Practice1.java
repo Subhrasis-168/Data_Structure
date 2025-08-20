7e1ea82aaa7f652d14ca354eb53aeb9b366a7f95
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-20 22:56:12 +0530

    Practice

---
+++ Practice/Practice1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+// User function Template for Java
+class Practice1 {
+
+    public static int find_greatest_number(int a, int b, int c) {
+        // Write your code here..
+        if(a >= b && a >= c){
+            return a;
+        }else if(b >= a && b >= c){
+            return b;
+        }else{
+        return c;
+        }
+    }
+}
