c3124ffef9083e7dd186265638e22c68ecdfb17f
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-20 22:58:22 +0530

    Practice

---
+++ Practice/Practice2.java
@@ -0,0 +1,10 @@
+package Practice;
+
+class Practice2 {
+    public static void utility(int x) {
+        // code here
+        for(int i = x; i >= 0 ;i--){
+            System.out.print(i+" ");
+        }
+    }
+}
