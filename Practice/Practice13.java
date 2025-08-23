4ad82f9961cb870e42b31b406409680c890aa6fa
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-23 20:05:32 +0530

    Practice

---
+++ Practice/Practice13.java
@@ -0,0 +1,11 @@
+package Practice;
+
+class Practice13{
+    public static int[] decrementArrayElements(int[] arr) {
+        // Code here
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = arr[i] - 1;
+        }
+        return arr;
+    }
+}
