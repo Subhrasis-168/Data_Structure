8dc56de95e4d9d3c7fd313d7a8f16dfcaafb9e89
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2025-08-23 20:04:05 +0530

    Practice

---
+++ Practice/Practice11.java
@@ -0,0 +1,11 @@
+package Practice;
+
+class Practice {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+
