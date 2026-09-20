f0830f6af27f47de6955b1afdaaa64fe54020cfd
Subhrasis Biswal <subhrasisbiswal753@gmail.com>
2026-09-20 23:25:32 +0530

    Delete ArratTraversal.java

--- ArratTraversal.java
+++
@@ -1,8 +0,0 @@
-class ArratTraversal {
-    public static void arrayTraversal(int[] arr) {
-        // Code here
-        for(int i = 0; i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-}
